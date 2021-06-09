82b8cbf16b6f9d97ad54c0f41f090be267f3d938
AdriRameti <adri7agu@gmail.com>
2021-06-09 19:51:23 +0200

    api rest details

--- frontend/modules/shop/controller/controller_details.js
+++ frontend/modules/shop/controller/controller_details.js
@@ -1,4 +1,11 @@
-kiwear.controller('controller_details', function($scope,services,details) {
-    console.log(details[0].img3);
+kiwear.controller('controller_details', function($scope,services,details,$http) {
+    console.log(details[0].nombre);
+
 $scope.details = details;
+$http.get("https://www.googleapis.com/books/v1/volumes?q="+details[0].nombre).then(function(data){
+    console.log(data);
+    $scope.apiDet = data.data.items;
+    console.log($scope.apiDet);
+
+});
 });
